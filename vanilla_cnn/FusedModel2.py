3674d638d225479dfe1c49fb8be160151c54db8f
feizhihui <helloworld@csu.edu.cn>
2017-12-04 14:34:10 +0800

    pkl2 version

--- vanilla_cnn/FusedModel2.py
+++ vanilla_cnn/FusedModel2.py
@@ -2,9 +2,9 @@
 import tensorflow as tf
 from tensorflow.contrib import layers
 
-embedding_size = 100
-doc_embedding_size = 128
-doc_feature_size = 64
+embedding_size = 128
+
+
 cnn_feature_size = 128
 
 time_steps = 700
@@ -34,7 +34,6 @@ class FusedModel(object):
         W = tf.Variable(embeddings, name="W", dtype=tf.float32)
         self.x = tf.placeholder(tf.int32, [None, time_steps])
         x_emb = tf.nn.embedding_lookup(W, self.x)
-        self.doc_x = tf.placeholder(tf.float32, [None, doc_embedding_size])
         self.y = tf.placeholder(tf.float32, [None, class_num])
         self.dropout_keep_prob = tf.placeholder(tf.float32, name="dropout_keep_prob")
 
@@ -44,24 +43,6 @@ class FusedModel(object):
 
         ones = tf.ones_like(self.y)
         zeros = tf.zeros_like(self.y)
-
-        with tf.name_scope("D2V_Part"):
-            doc_x_ = layers.fully_connected(self.doc_x, doc_feature_size,
-                                            weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
-                                            biases_initializer=tf.truncated_normal_initializer(stddev=0.1),
-                                            activation_fn=None)
-
-            doc_x_ = tf.nn.dropout(doc_x_, self.dropout_keep_prob)
-
-            logits_d2v = layers.fully_connected(doc_x_, class_num,
-                                                weights_initializer=tf.truncated_normal_initializer(stddev=0.1),
-                                                biases_initializer=tf.truncated_normal_initializer(stddev=0.1),
-                                                activation_fn=None)
-
-            self.loss_d2v = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y, logits=logits_d2v))
-            self.optimizer_d2v = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss_d2v)
-            self.score_d2v = tf.nn.sigmoid(logits_d2v)
-            self.prediction_d2v = tf.cast(tf.where(tf.greater(self.score_d2v, threshold), ones, zeros), tf.int32)
 
         with tf.name_scope("CNN_Part"):
             x_convs = tf.nn.dropout(x_convs, self.dropout_keep_prob)
@@ -79,23 +60,6 @@ class FusedModel(object):
             self.optimizer_cnn = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss_cnn)
             self.score_cnn = tf.nn.sigmoid(output)
             self.prediction_cnn = tf.cast(tf.where(tf.greater(self.score_cnn, threshold), ones, zeros), tf.int32)
-
-        with tf.name_scope("Fused_Part"):
-            # fn_input = tf.concat([logits_cnn, doc_x_], axis=1)
-            fn_input = logits_cnn
-            fn_input = tf.nn.dropout(fn_input, self.dropout_keep_prob)
-            # weights_f = tf.Variable(tf.truncated_normal([cnn_feature_size + doc_feature_size, class_num], stddev=0.1))
-            weights_f = tf.Variable(tf.truncated_normal([cnn_feature_size, class_num], stddev=0.1))
-
-            biases_f = tf.Variable(tf.truncated_normal([class_num], stddev=0.1))
-
-            logits_fused = tf.matmul(fn_input, weights_f) + biases_f
-
-            self.loss_fused = tf.reduce_mean(
-                tf.nn.sigmoid_cross_entropy_with_logits(labels=self.y, logits=logits_fused))
-            self.optimizer_fused = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.loss_fused)
-            self.score_fused = tf.nn.sigmoid(logits_fused)
-            self.prediction_fused = tf.cast(tf.where(tf.greater(self.score_fused, threshold), ones, zeros), tf.int32)
 
     def conv1d(sef, x, W, b):
         x = tf.reshape(x, shape=[-1, time_steps, embedding_size])
--- vanilla_cnn/main2.py
+++ vanilla_cnn/main2.py
@@ -1,7 +1,7 @@
 # encoding=utf-8
 import tensorflow as tf
 import data_input2
-import FusedModel2 as FusedModel2
+from FusedModel2 import FusedModel
 import numpy as np
 import os
 
@@ -10,26 +10,22 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "2"
 master = data_input2.data_master()
 
 batch_size = 256
-epoch_num_d2v = 15
-epoch_num_cnn = 25
-epoch_num_fused = 35
+epoch_num_cnn = 30
 keep_pro = 0.9
 
-model = FusedModel2.FusedModel(master.embeddings)
+model = FusedModel(master.embeddings)
 
 
 def validataion(model_prediction):
     # model.prediction_fused
     print('begin to test:')
     test_labels = master.change_multi_hot(master.test_labels)
-    step_size = 300
     outputs = []
-    for i in range(0, len(master.test_sentences), step_size):
-        test_sentences_batch = master.test_sentences[i:i + step_size]
-        test_docs_batch = master.test_docs[i:i + step_size]
-        test_labels_batch = test_labels[i:i + step_size]
+    for i in range(0, len(master.test_sentences), batch_size):
+        test_sentences_batch = master.test_sentences[i:i + batch_size]
+        test_labels_batch = test_labels[i:i + batch_size]
         output = sess.run(model_prediction,
-                          feed_dict={model.x: test_sentences_batch, model.doc_x: test_docs_batch,
+                          feed_dict={model.x: test_sentences_batch,
                                      model.y: test_labels_batch, model.dropout_keep_prob: 1.0})
         outputs.append(output)
 
@@ -55,7 +51,6 @@ def micro_score(output, label):
 
 
 def macro_score(output, label):
-
     total_P = np.sum(output, axis=0)
     total_R = np.sum(label, axis=0)
     TP = np.sum(output * label, axis=0)
@@ -67,24 +62,10 @@ def macro_score(output, label):
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    # print('pretraining D2V Part')
-    # for epoch in range(epoch_num_d2v):
-    #     master.shuffle()
-    #     for iter, (batch_x, batch_docx, batch_y) in enumerate(master.batch_iter(batch_size)):
-    #         loss_fetch, output, _ = sess.run([model.loss_d2v, model.prediction_d2v, model.optimizer_d2v],
-    #                                          feed_dict={model.doc_x: batch_docx, model.y: batch_y,
-    #                                                     model.dropout_keep_prob: keep_pro})
-    #         if iter % 100 == 0:
-    #             print("===D2VPart===")
-    #             MiP, MiR, MiF, P_NUM, T_NUM = micro_score(output, batch_y)
-    #             print("epoch:%d  iter:%d, mean loss:%.3f,  PNum:%.2f, TNum:%.2f" % (
-    #                 epoch + 1, iter + 1, loss_fetch, P_NUM, T_NUM))
-    #             print("Micro-Precision:%.3f, Micro-Recall:%.3f, Micro-F Measure:%.3f" % (MiP, MiR, MiF))
-    # validataion(model.prediction_d2v)
     print('pretraining CNN Part')
     for epoch in range(epoch_num_cnn):
         master.shuffle()
-        for iter, (batch_x, batch_docx, batch_y) in enumerate(master.batch_iter(batch_size)):
+        for iter, (batch_x, batch_y) in enumerate(master.batch_iter(batch_size)):
             loss_fetch, output, _ = sess.run([model.loss_cnn, model.prediction_cnn, model.optimizer_cnn],
                                              feed_dict={model.x: batch_x, model.y: batch_y,
                                                         model.dropout_keep_prob: keep_pro})
@@ -94,21 +75,5 @@ with tf.Session() as sess:
                 print("epoch:%d  iter:%d, mean loss:%.3f,  PNum:%.2f, TNum:%.2f" % (
                     epoch + 1, iter + 1, loss_fetch, P_NUM, T_NUM))
                 print("Micro-Precision:%.3f, Micro-Recall:%.3f, Micro-F Measure:%.3f" % (MiP, MiR, MiF))
-    validataion(model.prediction_cnn)
-
-
-    # print('pretraining Fused Part')
-    # for epoch in range(epoch_num_fused):
-    #     master.shuffle()
-    #     for iter, (batch_x, batch_docx, batch_y) in enumerate(master.batch_iter(batch_size)):
-    #         loss_fetch, output, _ = sess.run([model.loss_fused, model.prediction_fused, model.optimizer_fused],
-    #                                          feed_dict={model.x: batch_x, model.doc_x: batch_docx, model.y: batch_y,
-    #                                                     model.dropout_keep_prob: keep_pro})
-    #         if iter % 100 == 0:
-    #             print("===FusedPart===")
-    #             MiP, MiR, MiF, P_NUM, T_NUM = micro_score(output, batch_y)
-    #             print("epoch:%d  iter:%d, mean loss:%.3f,  PNum:%.2f, TNum:%.2f" % (
-    #                 epoch + 1, iter + 1, loss_fetch, P_NUM, T_NUM))
-    #             print("Micro-Precision:%.3f, Micro-Recall:%.3f, Micro-F Measure:%.3f" % (MiP, MiR, MiF))
-    #     if epoch >= 20:
-    #         validataion(model.prediction_fused)
+        if epoch >= 20:
+            validataion(model.prediction_cnn)
